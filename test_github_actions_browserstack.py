efa7f5cbdfd35ec8db355774f94e899595e06ece
wangcong <wangcong@gzkj.com>
2021-03-23 11:49:00 +0800

    第一次

---
+++ test_github_actions_browserstack.py
@@ -0,0 +1,22 @@
+from selenium import webdriver
+from threading import Thread
+caps = [
+    {
+    'os_version': '10',
+    'os': 'Windows',
+    'browser': 'chrome',
+    'browser_version': '86.0',
+    'name': 'gg1', 
+    'build': 'ggCase_bulid',
+    },
+
+]
+username =  'wangcong1'
+accessKey = 'cDEx8QD3UMfxCnVuAE9H'
+
+def test(cap):
+    driver = webdriver.Remote('https://'+username+':'+accessKey+'@hub-cloud.browserstack.com/wd/hub', desired_capabilities=cap)
+    driver.get('https://www.google.com')
+
+for cap in caps:
+    Thread(target=test, args=(cap,)).start()  
